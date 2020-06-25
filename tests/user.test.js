24b068e5ec4c711b28ff0d6f1692de195cad591b
Rutul Darji <rutul.darji@vuclip.com>
2020-06-25 21:36:41 +0530

    added testcases

---
+++ tests/user.test.js
@@ -0,0 +1,11 @@
+const request = require('supertest')
+const app = require('../src/app')
+
+test('Should sign up a new user', async () => {
+    await request(app).post('/users')
+    .send({
+        name: 'xyz',
+        email: 'xzy@gmail.com',
+        password: 'xzy@123'
+    }).expect(201)
+})
